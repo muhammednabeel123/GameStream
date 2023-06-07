217f2e5452731fb001ccb1202cceb1184edabcf3
muhammednabeel123 <muhammednabeel237@gmail.com>
2023-06-08 01:52:13 +0530

    form validation

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -2,16 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthModalComponent } from './auth-modal/auth-modal.component';
 import { SharedModule } from '../shared/shared.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 
 
 @NgModule({
   declarations: [
-    AuthModalComponent
+    AuthModalComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     CommonModule,
-    SharedModule
+    SharedModule,
+    ReactiveFormsModule
   ],
   exports:[
     AuthModalComponent
