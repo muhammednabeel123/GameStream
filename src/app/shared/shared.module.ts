0786e4e46d0bb7bd8824ee56fe9344a8619264e6
muhammednabeel123 <muhammednabeel237@gmail.com>
2023-06-08 19:19:13 +0530

    ngx mask

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,6 +3,10 @@ import { CommonModule } from '@angular/common';
 import { ModalComponent } from './modal/modal.component';
 import { TabsContainerComponent } from './tabs-container/tabs-container.component';
 import { TabComponent } from './tab/tab.component';
+import { InputComponent } from './input/input.component';
+import { ReactiveFormsModule } from '@angular/forms';
+// import { provideEnvironmentNgxMask,NgxMaskDirective } from 'ngx-mask';
+
 //  
 
 
@@ -11,15 +15,19 @@ import { TabComponent } from './tab/tab.component';
   declarations: [
     ModalComponent,
     TabsContainerComponent,
-    TabComponent
+    TabComponent,
+    InputComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule,
   ],
   exports:[
     ModalComponent,
     TabsContainerComponent,
-    TabComponent
+    TabComponent,
+    InputComponent
   ],
+
 })
 export class SharedModule { }
--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -7,17 +7,45 @@ import { FormControl, FormGroup,Validator, Validators } from '@angular/forms';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
-  registerForm = new FormGroup({
-    name :  new FormControl('',[
-      Validators.required,
-    
-    ]),
-    email : new FormControl(''),
-    age : new FormControl(''),
-    password : new FormControl(''),
-    confirm_password : new FormControl(''),
-    phoneNumber : new FormControl('')
+  name =  new FormControl('',[
+    Validators.required,
+    Validators.minLength(3)
+  
+  ])
+  email = new FormControl('',[
+    Validators.required,
+    Validators.email
+  ])
+  age = new FormControl('',[
+    Validators.required,
+    Validators.min(18),
+    Validators.max(120)
+  ])
+  password = new FormControl('',[
+    Validators.required,
+    Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm)
+  ])
+  confirm_password = new FormControl('',[
+    Validators.required,
+  
+  ])
+  phoneNumber = new FormControl('',[
+    Validators.required,
+    Validators.minLength(13),
+    Validators.maxLength(13)
+  ])
 
+
+  registerForm = new FormGroup({
+
+    name: this.name,
+    email : this.email,
+    age : this.age,
+    password : this.password,
+    confirm_password : this.confirm_password,
+    phoneNumber : this.phoneNumber
+
+  
 
   })
 
